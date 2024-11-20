426994588e1b5b4826e2521059187b1fad8c29fc
Aykut Akgün <aykut.akguen@it-suisse.com>
2024-11-20 17:31:24 +0100

    fix(1070): fixing wrong name

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -2234,7 +2234,7 @@ gulp.task(
 function packageJson() {
   const VERSION = getVersionJSON().version;
 
-  const DIST_NAME = "pdfjs-dist";
+  const DIST_NAME = "@doinstruct/pdf.js";
   const DIST_DESCRIPTION = "Generic build of Mozilla's PDF.js library.";
   const DIST_KEYWORDS = ["Mozilla", "pdf", "pdf.js"];
   const DIST_HOMEPAGE = "https://mozilla.github.io/pdf.js/";
